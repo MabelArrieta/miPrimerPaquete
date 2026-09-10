119991573fad20a00ee09ae9b04d2c75ebd046b3
sama <saida.mabel@gmail.com>
2026-09-10 18:15:38 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miPrimerPaquete",                                 # Nombre del paquete
+    version="0.1.0",                                        # Versión inicial
+    packages=find_packages(),                               # Paquetes a incluir
+    description="Un paquete pip simple de saludo",          # Breve descripción
+    author="Mabel Arrieta",                                 # Tu nombre
+    author_email="saida.mabel@gmail.com",                   # Tu correo electrónico
+    url="https://github.com/MabelArrieta/miPrimerPaquete",  # URL del proyecto
+)
